af0a6406ff40bbb8cb90734f974bcaec45fcb3cc
Ailton Costa <166161526+acadl-dev@users.noreply.github.com>
2025-07-23 08:10:33 -0300

    Adição da função para calcular o IMC. Button calcular funcionando

--- onebithealth/src/components/Form/index.js
+++ onebithealth/src/components/Form/index.js
@@ -8,7 +8,26 @@ export default function Form(){
     const [weight, setWeight]= useState(null)
     const [messageImc, setMessageImc]= useState('Preencha o peso e a altura')
     const [imc, setImc]= useState(null)
-    const [textButton, setTextB]= useState('Calcular')
+    const [textButton, setTextButton]= useState('Calcular')
+
+    function imcCalculator() {
+        return setImc((weight / (height * height)).toFixed(2));
+    }
+
+    function validationImc() {
+        if (weight != null && height != null) {
+            imcCalculator();
+            setHeight(null);
+            setWeight(null);
+            setMessageImc("Seu IMC é igual: ");
+            setTextButton("Calcular Novamente");
+            return;
+        }
+        setImc(null);
+        setTextButton("Calcular");
+        setMessageImc("Preencha o peso e a altura");
+    }
+
 
     return(
         <View>
@@ -16,17 +35,24 @@ export default function Form(){
                 <Text>Altura</Text>
 
                 <TextInput 
-                placeholder='Ex. 75.365'
-                keyboardType='numeric'
+                    onChangeText={setHeight}
+                    value={height}
+                    placeholder='Ex. 1.75'
+                    keyboardType='numeric'
                 />
 
                 <Text>Peso</Text>
 
                 <TextInput
-                 placeholder='Ex. 1.75'
-                keyboardType='numeric'/>
+                    onChangeText={setWeight}
+                    value={weight}
+                    placeholder='Ex. 75.365'
+                    keyboardType='numeric'/>
 
-                <Button title='Calcular IMC'/>
+                <Button 
+                    onPress={() => validationImc()}
+                    title={textButton}
+                />
 
                 </View>
             <ResultImc messageResultImc={messageImc} resultImc={imc} />
